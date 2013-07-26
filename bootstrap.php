dbaf27aafc3e6d9e878618949328ffb3b0505e6e
Alex Groleau <alex@teampalmtree.com>
2013-07-25 21:52:04 -0400

    added classes

---
+++ bootstrap.php
@@ -0,0 +1,5 @@
+<?php
+
+Autoloader::add_classes(array(
+    'Controller_Shared'   => __DIR__.'/classes/controller/shared.php',
+));
---
+++ classes/controller/shared.php
@@ -0,0 +1,22 @@
+<?php
+
+class Controller_Shared extends Controller_Hybrid
+{
+    public $section;
+
+    public function before()
+    {
+        // call parent before required
+        parent::before();
+
+        //////////////////////////////
+        // SET TIMEZONE INFORMATION //
+        //////////////////////////////
+
+        // set global timezones
+        Helper::$server_timezone = new DateTimeZone(Config::get('default_timezone'));
+        Helper::$user_timezone = new DateTimeZone('America/New_York');
+
+    }
+
+}
