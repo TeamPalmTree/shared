64008cbe2bf4e486525e1a927b7eaed3f25198d7
Alex Groleau <alex@teampalmtree.com>
2013-07-28 12:23:54 -0400

    template updates

--- bootstrap.php
+++ bootstrap.php
@@ -1,5 +1,8 @@
 <?php
 
+// add views folder
+Finder::instance()->add_path(__DIR__);
+
 Autoloader::add_classes(array(
     'Controller_Shared'   => __DIR__.'/classes/controller/shared.php',
 ));
---
+++ views/network/network.php
@@ -0,0 +1,23 @@
+<div class="navbar navbar-inverse navbar-fixed-top">
+    <div class="navbar-inner">
+        <div class="container">
+            <a class="btn btn-navbar" data-toggle="collapse" data-target="#network-collapse">
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </a>
+            <a class="tpt_navigation-emblem" href="#"></a>
+            <a class="brand" href="#">Team Palm Tree</a>
+            <div id="network-collapse" class="nav-collapse collapse">
+                <ul class="nav">
+                    <li class="dropdown">
+                        <a href="#" class="dropdown-toggle">PROJECTS</b></a>
+                        <ul class="dropdown-menu">
+                            <li><a href="#">GDM Radio</a></li>
+                        </ul>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</div>
