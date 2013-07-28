c0e76cf9124022ba690c745d0611ab47f0ac457c
Alex Groleau <alex@teampalmtree.com>
2013-07-28 17:14:51 -0400

    fixed shared assets

--- bootstrap.php
+++ bootstrap.php
@@ -4,9 +4,9 @@
 Finder::instance()->add_path(__DIR__);
 
 // add shared assets
-Asset::instance()->add_path(DOCROOT.'shared/js', 'js');
-Asset::instance()->add_path(DOCROOT.'shared/img', 'img');
-Asset::instance()->add_path(DOCROOT.'shared/css', 'css');
+Asset::instance()->add_path(DOCROOT.'assets/shared/js', 'js');
+Asset::instance()->add_path(DOCROOT.'assets/shared/img', 'img');
+Asset::instance()->add_path(DOCROOT.'assets/shared/css', 'css');
 
 Autoloader::add_classes(array(
     'Controller_Shared'   => __DIR__.'/classes/controller/shared.php',
