8f3af64b1237d939211c63313e279a7fb8ef2797
Alex Groleau <alex@teampalmtree.com>
2013-07-31 21:40:06 -0400

    updated shared code

--- classes/controller/shared.php
+++ classes/controller/shared.php
@@ -19,4 +19,25 @@ class Controller_Shared extends Controller_Hybrid
 
     }
 
+    public function router($method, $params)
+    {
+
+        ////////////////////
+        // TEMPLATE SETUP //
+        ////////////////////
+
+        // if we aren't restful and aren't passing a REST key
+        // set up the template for the UI
+        if (!$this->is_restful())
+        {
+            $this->template->network = View::forge('shared/network');
+            $this->template->header = View::forge('shared/header');
+            $this->template->footer = View::forge('shared/footer');
+        }
+
+        // forward to FPHP router
+        parent::router($method, $params);
+
+    }
+
 }
