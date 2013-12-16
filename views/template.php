8288fb57dff3597028b2608685c02396cf8a0bb2
Alex Groleau <alex@teampalmtree.com>
2013-12-16 00:06:32 -0500

    shared less and template updates (standardization)

--- views/template.php
+++ views/template.php
@@ -1,4 +1,5 @@
 <!DOCTYPE html>
+<html>
 <head>
     <meta charset="utf-8">
     <title>
@@ -9,26 +10,33 @@
         <?php endif; ?>
     </title>
     <!-- viewport -->
-    <meta name="viewport" content="width=device-width">
-    <!-- icon -->
-    <link rel="icon" type="image/vnd.Microsoft.icon" href="/assets/img/icon.ico" />
-    <!-- less -->
+    <meta name="viewport" content="width=device-width, initial-scale=0.8, user-scalable=no">
+    <!-- css -->
     <?php echo Asset::css('reset.css'); ?>
     <?php echo Asset::css('template.css'); ?>
-    <!-- scripts -->
+    <!-- js -->
     <?php echo Asset::js('jquery.min.js'); ?>
-    <?php echo Asset::js('jquery-ui.min.js'); ?>
+    <?php echo Asset::js('dateformat.js'); ?>
     <?php echo Asset::js('bootstrap.min.js'); ?>
+    <?php echo Asset::js('bootstrap-datetimepicker.min.js'); ?>
+    <?php echo Asset::js('typeahead.min.js'); ?>
     <?php echo Asset::js('knockout.min.js'); ?>
     <?php echo Asset::js('knockout.mapping.min.js'); ?>
+    <?php echo Asset::js('knockout.bindingHandlers.js'); ?>
+    <?php echo Asset::js('knockout-bootstrap.min.js'); ?>
+    <?php echo Asset::js('knockout.orderable.js'); ?>
+    <?php echo Asset::js('jquery-ui.min.js'); ?>
+    <?php echo Asset::js('jquery.tablesorter.min.js'); ?>
+    <?php echo Asset::js('knockout-sortable.min.js'); ?>
+    <?php echo Asset::js('helper.js'); ?>
     <?php echo Asset::js('template.js'); ?>
 </head>
 <body>
-<?php echo $modal; ?>
-<div class="standard">
-    <?php if (isset($display)) echo $display; ?>
-    <?php echo $navigation; ?>
-    <?php echo $section; ?>
-</div>
+    <?php echo $modal; ?>
+    <div class="standard">
+        <?php if (isset($display)) echo $display; ?>
+        <?php echo $navigation; ?>
+        <?php echo $section; ?>
+    </div>
 </body>
 </html>
