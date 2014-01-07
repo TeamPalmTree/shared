9c6a630629f082961fea4634226d2d2a92618f7f
Alex Groleau <alex@teampalmtree.com>
2014-01-07 12:57:36 -0500

    standard controller router order issues corrected

--- classes/controller/standard.php
+++ classes/controller/standard.php
@@ -9,15 +9,14 @@ class Controller_Standard extends Controller_Shared
     public function router($method, $params)
     {
 
+        // call parent before required
+        parent::router($method, $params);
         // set up standard template
         if (is_object($this->template))
         {
             $this->template->modal = View::forge('standard/modal');
             $this->template->section = View::forge('standard/section');
         }
-
-        // call parent before required
-        parent::router($method, $params);
 
     }
 
